132770b567d2f27eed5c5b99ca07e918d74bd7ca
brandoly-jack <143424064+brandoly-jack@users.noreply.github.com>
2023-11-03 21:25:17 -0500

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Home() {
+
+  const [ rotateTransition, setRotateTransition ] = useState(false)
   return (
     <section className='container-home'>
       <div className='container-spotify-title'>
@@ -10,13 +13,13 @@ function Home() {
       </div>
       <div className='container-lists-img-home'>
         <ul className='lists-img-home'>
-            <li  className='list1'>
+            <li  className={ rotateTransition?'list1 list1next':'list1' }>
                 <img src="./../../public/2_2.jpg" alt="" />
             </li>
-            <li className='list2'>
+            <li className={ rotateTransition?'list2 list2next':'list2' }>
                 <img src="../../public/2_1.jpg" alt="" />
             </li>
-            <li className='list3'>
+            <li className={ rotateTransition?'list3 list3next':'list3' } onMouseEnter={() => setRotateTransition(true)} onMouseLeave={() => setRotateTransition(false)}>
                 <img src="../../public/card3_3.jpg" alt="" />
             </li>
         </ul>
