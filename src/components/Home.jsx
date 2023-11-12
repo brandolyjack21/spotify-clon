ff9f6c32b596434d88965994cb30253fbf96482c
brandoly-jack <143424064+brandoly-jack@users.noreply.github.com>
2023-11-11 22:56:44 -0500

    home completado con search

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,20 +7,23 @@ function Home() {
   return (
     <section className='container-home'>
       <div className='container-spotify-title'>
-        <h1>
-            Spotify
-        </h1>
+        <div className='container-search-music'>
+          <input className='input-search-music' id='search-music' type="text" />
+          <label htmlFor="search-music"><i class='bx bx-search'></i></label>
+        </div>
       </div>
       <div className='container-lists-img-home'>
+        <div className='container-banner-circle' onMouseEnter={() => setRotateTransition(true)} onMouseLeave={() => setRotateTransition(false)}>
+        </div>
         <ul className='lists-img-home'>
             <li  className={ rotateTransition?'list1 list1next':'list1' }>
-                <img src="./../../public/2_2.jpg" alt="" />
+                <img src="./../../public/2_1.jpg" alt="" />
             </li>
             <li className={ rotateTransition?'list2 list2next':'list2' }>
-                <img src="../../public/2_1.jpg" alt="" />
+                <img src="../../public/card3_3.jpg" alt="" />
             </li>
             <li className={ rotateTransition?'list3 list3next':'list3' } onMouseEnter={() => setRotateTransition(true)} onMouseLeave={() => setRotateTransition(false)}>
-                <img src="../../public/card3_3.jpg" alt="" />
+                <img src="../../public/2_2.jpg" alt="" />
             </li>
         </ul>
 
